0aeb07965e082c1beacca663353bcf7aadb3f936
Joshua Martin <josh@hive05.com>
2016-06-06 18:33:33 -0700

    Basic bot is functional.

--- DiscordWeatherBot/Program.cs
+++ DiscordWeatherBot/Program.cs
@@ -1,24 +1,31 @@
-﻿using ServiceStack;
-using ServiceStack.Text;
+﻿using Discord;
+using ServiceStack;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordWeatherBot
 {
     class Program
     {
+        //client id is the public client id of your bot, it can be found here:
+        //https://discordapp.com/developers/applications/me
+
+        //goto this URL to add your bot to your server:
+        //https://discordapp.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&scope=bot&permissions=19456
+
         static string wundergroundKey = "";
+        static string discordKey = "";
+
+        static DiscordClient client;
 
         async static Task<CurrentObservation> GetWeather(string postalCode)
         {
             var requestUrl = $"http://api.wunderground.com/api/{wundergroundKey}/conditions/q/{postalCode}.json";
+            Debug.WriteLine($"Getting the weather using url: {requestUrl}");
             using (var client = new HttpClient())
             {
                 try
@@ -37,16 +44,41 @@ namespace DiscordWeatherBot
 
         static void Main(string[] args)
         {
-            using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DiscordWeatherBot.Api.key")))
+            using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DiscordWeatherBot.weather.key")))
                 wundergroundKey = reader.ReadToEnd();
-
             Debug.WriteLine($"Found Weather Underground Key: {wundergroundKey}");
 
-            var weatherTask = GetWeather("89147");
-            weatherTask.Wait();
+            using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DiscordWeatherBot.discord.key")))
+                discordKey = reader.ReadToEnd();
+            Debug.WriteLine($"Found Discord Token: {discordKey}");
 
-            var weather = weatherTask.Result;
-            Console.Out.WriteLine($"The weather in {weather.display_location.full} is now: {weather.temp_c} ({weather.temp_f} F)");
+            client = new DiscordClient();
+            client.MessageReceived += async (s,e) =>
+            {
+                const string weatherCommand = "!weather";
+
+                var lower = e.Message.RawText.ToLower().Trim();
+                var isWeatherRequest = lower.StartsWith(weatherCommand);
+                if( isWeatherRequest )
+                {
+                    var location = lower.Substring(weatherCommand.Length);
+                    if( location.Length > 0 )
+                    {
+                        var results = await GetWeather(location.Trim());
+                        var message = $"Unable to locate weather for: \"{location}";
+                        if (results != null)
+                            message = $"The weather in {results.display_location.full} is now: {results.temp_c} ({results.temp_f} F)";
+
+                        await e.Channel.SendMessage(message);
+                    }
+                }
+            };
+
+            Console.WriteLine("Connecting to Discord...");
+            var connectTask = client.Connect(discordKey);
+            connectTask.Wait();
+
+
             Console.In.ReadLine();
         }
     }
