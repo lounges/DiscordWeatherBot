561bd5aa89158dd5a20ac81eaa617b77d1489569
Joshua Martin <josh@hive05.com>
2016-06-28 21:52:20 -0700

    Integrated buster bot.

--- DiscordWeatherBot/Program.cs
+++ DiscordWeatherBot/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using ServiceStack;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
@@ -11,6 +12,44 @@ namespace DiscordWeatherBot
 {
     class Program
     {
+        public static List<string> bustedImages = new List<string>
+        {
+            "Images/busted1.jpg",
+            "Images/busted2.jpg",
+            "Images/busted3.jpg",
+            "Images/busted4.jpg",
+            "Images/busted5.png",
+            "Images/busted6.jpg",
+            "Images/busted7.jpg",
+            "Images/busted8.jpg",
+            "Images/busted9.jpg",
+            "Images/busted10.jpg",
+            "Images/busted11.jpg",
+        };
+
+        public static List<string> confirmedImages = new List<string>
+        {
+            "Images/confirmed1.jpg",
+            "Images/confirmed2.jpg",
+            "Images/confirmed3.jpg",
+            "Images/confirmed4.jpg",
+            "Images/confirmed5.jpg",
+            "Images/confirmed6.jpg",
+            "Images/confirmed7.jpg",
+            "Images/confirmed8.jpg",
+            "Images/confirmed9.jpg",
+        };
+
+        public static List<string> plausibleImages = new List<string>
+        {
+            "Images/plausible1.jpg",
+            "Images/plausible2.jpg",
+            "Images/plausible3.jpg",
+            "Images/plausible4.jpg",
+            "Images/plausible5.jpg",
+            "Images/plausible6.jpg",
+        };
+
         //client id is the public client id of your bot, it can be found here:
         //https://discordapp.com/developers/applications/me
 
@@ -19,6 +58,7 @@ namespace DiscordWeatherBot
 
         static string wundergroundKey = "";
         static string discordKey = "";
+        
 
         static DiscordClient client;
 
@@ -44,6 +84,8 @@ namespace DiscordWeatherBot
 
         static void Main(string[] args)
         {
+            Random r = new Random();
+
             using (var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("DiscordWeatherBot.weather.key")))
                 wundergroundKey = reader.ReadToEnd();
             Debug.WriteLine($"Found Weather Underground Key: {wundergroundKey}");
@@ -65,24 +107,44 @@ namespace DiscordWeatherBot
                     if( location.Length > 0 )
                     {
                         var results = await GetWeather(location.Trim());
-                        var message = $"Unable to locate weather for: \"{location}";
+                        var message = $"Unable to locate weather for: \"{location}\"";
                         if (results != null)
-                            message = $"The weather in {results.display_location.full} is now: {results.temp_c} ({results.temp_f} F)";
+                            message = $"The temperature in {results.display_location.full} is now: {results.temp_c} ({results.temp_f} F)\r\nHumidity: {results.relative_humidity}\r\nWinds {results.wind_string}\r\nIt is presently: {results.weather}\r\n";
 
                         await e.Channel.SendMessage(message);
-
-
-                        
-
                     }
                 }
+
+
+
+
+
+
+                const string confirmedCommand = "!confirmed";
+                const string bustedCommand = "!busted";
+                const string plausibleCommand = "!plausible";
+
+                lower = e.Message.RawText.ToLower().Trim();
+
+                var isConfirmed = lower.StartsWith(confirmedCommand);
+                var isBusted = lower.StartsWith(bustedCommand);
+                var isPlausible = lower.StartsWith(plausibleCommand);
+
+                if (isConfirmed)
+                    await e.Channel.SendFile(confirmedImages[r.Next(confirmedImages.Count)]);
+                else if (isBusted)
+                    await e.Channel.SendFile(bustedImages[r.Next(bustedImages.Count)]);
+                else if (isPlausible)
+                    await e.Channel.SendFile(plausibleImages[r.Next(plausibleImages.Count)]);
+
+
             };
 
             Console.WriteLine("Connecting to Discord...");
             var connectTask = client.Connect(discordKey);
             connectTask.Wait();
             client.SetStatus(UserStatus.Online);
-            client.SetGame("WeatherUnderground");
+            client.SetGame("ChicaChica");
 
 
             Console.In.ReadLine();
